4ec9a000c3cfc094e6a7ef5edd5f56dee56cdaab
WSLubuntu <1339903191@qq.com>
2024-12-19 14:01:30 +0800

    实现了sort方法

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void srot(int arr[], int size)
+void sort(int arr[], int size)
 {
+    int tmp = 0;
+    for (int i = 0; i < size - 1; ++i)
+    {
+        for (int j = 0; j < size - 1; ++j)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
 
 }
 
