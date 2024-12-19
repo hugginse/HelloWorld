41c43aef38b340ff0b0c4089ffa27bc774d10a7c
Huggins <1339903191@qq.com>
2024-12-19 15:38:08 +0800

    修改sort算法为快排算法

--- main.cpp
+++ main.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
 using namespace std;
 
+void sort(int arr[], int i , int j)
+{
+    int val = arr[i];
+    int l = i; 
+    int r = j;
+    while (l < r)
+    {
+        while (l < r && arr[r] >= val)
+        {
+            r--;
+        }
+        if (l < r)
+        {
+            arr[l++] = arr[r];
+        }
+        while (l < r && arr[l] <= val)
+        {
+            l++;
+        }
+        if (l < r)
+        {
+            arr[r--] = arr[l];
+        }
+    }
+    arr[l] = val;
+    sort(arr, i, l - 1);
+    sort(arr, l+1, j);
+}
 // WSLubuntu给sort添加注释
 void sort(int arr[], int size)
 {
-    int tmp = 0;
-    char flag = false;
-    for (int i = 0; i < size - 1; ++i)
-    {
-        for (int j = 0; j < size - 1; ++j)
-        {
-            // Windows修改排序算法，从大到小排序
-            if (arr[j] < arr[j + 1])
-            {
-                tmp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = tmp;
-                flag = true;
-            }
-        }
-        if (!flag)
-            break;
-    }
-
+   sort(arr, 0, size);
 }
 
 int main()
@@ -32,5 +42,10 @@ int main()
     int size = sizeof(arr)/sizeof(arr[0]);
 
     sort(arr, size);
+    for (int i = 0; i < size; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
